4027d1faacc49b3ba39e658363bed78a21f2ed39
nou <1328116700@qq.com>
2024-01-06 21:46:58 +0800

    add tip

--- src/mod/core/init.lua
+++ src/mod/core/init.lua
@@ -1,3 +1,6 @@
 loadfile("mod/core/initModManager.lua", "bt")()
 loadfile("mod/core/loadLib.lua", "bt")()
+
+print("loading mods ...")
 loadfile("mod/core/loadMods.lua", "bt")()
+print("loading complete")
